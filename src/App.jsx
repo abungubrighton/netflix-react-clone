686d4fd4ba64b74848efceeb0e9631c8ff7b1fc9
abungubrighton <brighton@ibuqacapital.com>
2023-10-09 09:02:40 +0300

    chore: fetch row data

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css';
-
+import Row from './components/Row';
+import requests from './requests';
 function App() {
-    return ( <
-        div className = "App" >
-        Hello
+    return ( 
+        <div className = "App" >
+        <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals}/>
+        <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
         </div>
     );
 }
